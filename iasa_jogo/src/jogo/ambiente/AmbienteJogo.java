b06d5db9cc012a6773892ec0b66816b36a2e5af0
Vectoria <benjamim.noronha60@gmail.com>
2024-02-28 23:32:51 +0000

    por agr assim (antes da segunda aula)

--- iasa_jogo/src/jogo/ambiente/AmbienteJogo.java
+++ iasa_jogo/src/jogo/ambiente/AmbienteJogo.java
@@ -4,9 +4,13 @@ import ambiente.Ambiente;
 import ambiente.Comando;
 import ambiente.Evento;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AmbienteJogo implements Ambiente {
     private EventoJogo evento;
-    private Evento[] eventos= new Evento[10];
+    //private Evento[] eventos= new Evento[10];
+    private List<Evento> eventos=new ArrayList<Evento>();
     private ComandoJogo comandoJogo;
 
     public AmbienteJogo() {
